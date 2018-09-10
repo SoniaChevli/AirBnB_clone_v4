1751893c881d1f0724a2d256472083571112443e
maupinpamela <maupinpamela@gmail.com>
2018-09-10 05:17:20 +0000

    work in progress based on 3-hbnb.js

---
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -0,0 +1,49 @@
+let amenityChecked = [];
+
+$("document").ready(function() {
+  $(".amenities input").change(function() {
+    if ($(this).prop("checked")) {
+      amenityChecked.push($(this).attr("data-name"));
+    } else {
+      let name = $(this).attr("data-name");
+      let index = amenityChecked.indexOf(name);
+      if (index > -1) {
+        amenityChecked.splice(index, 1);
+      }
+    }
+    amenityChecked.sort();
+    $(".amenities h4").text(amenityChecked.join(", "));
+  });
+  $.getJSON("http://0.0.0.0:5001/api/v1/places_search", function(status, data) {
+    if (status.status === "OK") {
+      $("DIV#api_status").addClass("available");
+    } else {
+      $("DIV#api_status").removeClass("available");
+    }
+    console.log("HERE", data);
+  });
+  $.ajax({
+    url: "http://0.0.0.0:5001/api/v1/places_search",
+    type: "POST",
+    dataType: "json",
+    contentType: "application/json",
+    data: "{}"
+  });
+
+  $.post(
+    "http://0.0.0.0:5001/api/v1/places_search", // url
+    { myData: "{}" }, // data to be submit
+    function(data) {
+      $("SECTION.places").append("<ARTICLE>" + data + "</ARTICLE>");
+    }
+  );
+
+    $('somebutton').click(function() {
+	$.ajax({
+	    type: 'POST',
+	    url: 'http://0.0.0.0:5001/api/v1/places_search/',
+	    data: JSON.stringify({'amenities'}),
+	    contentType: 'application/json',
+	    success: function (data) {
+		$('SECTION.[places').empty();
+});
